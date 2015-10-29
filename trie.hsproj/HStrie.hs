992afbccb60856e331c311848745c8578e23c380
oisdk <oisin.kidney@gmail.com>
2015-10-29 20:12:54 +0000

    searches

--- trie.hsproj/HStrie.hs
+++ trie.hsproj/HStrie.hs
@@ -8,92 +8,53 @@ import Control.Monad
 
 data Trie a = Trie { getTrie :: M.Map a (Trie a), endHere :: Bool } deriving (Eq)
 
-instance Show a => Show (Trie a) where
-  show  = show . toList
-
--- | overMap applies its first argument to the Map in the Trie of its second
--- argument.
-overMap :: Ord b => (M.Map a (Trie a) -> M.Map b (Trie b)) -> Trie a -> Trie b
-overMap f (Trie m e) = Trie (f m) e
-
-
--- | zipUntil constructs a function that acts over a foldable and a Trie.
--- The function constructed will follow the Trie and the foldable together,
--- until either the next element can't be found in the Trie, so it evaluates
--- to its first argument, or the foldable is exhausted, so it evaluates its
--- second argument on the nested Trie it finished on.
-zipUntil :: (Ord a, Foldable f) => b -> (Trie a -> b) -> f a -> Trie a -> b
-zipUntil base = foldr f where
-  f e a = fromMaybe base . fmap a . M.lookup e . getTrie 
-  
--- | Is True iff the foldable is contained in the Trie
-contains :: (Ord a, Foldable f) => f a -> Trie a -> Bool
-contains = zipUntil False endHere
-
-containsL :: Eq a => [a] -> [[a]] -> Bool
-containsL = any . (==)
-
--- | Evaluates to a Trie of the completions of the foldable
-complete :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
-complete = zipUntil empty id
-
-completeL :: (Ord a) => [a] -> [[a]] -> Trie a
-completeL l = fromList . map (drop (length l)) . filter (L.isPrefixOf l)
-
--- | Checks that a function on a trie and a function on a list 
--- of lists is equivalent
-check :: (Ord a, Eq c) => ([a] -> Trie a -> c) -> ([a] -> [[a]] -> c) -> [[a]] -> Bool
-check tf lf l = all (liftM2 (==) tfa lfa) (liftM2 (++) L.tails L.inits =<< l)  where
-  t   = fromList l
-  tfa = flip tf t
-  lfa = flip lf l
-  
--- | Evaluates to a Trie of all of the members which begin with
--- the foldable
-begins :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
-begins xs = toTrie . begins' xs . Just where
-  begins' = foldr f id
-  f e a   = (fmap (flip Trie False . M.singleton e) . a . M.lookup e . getTrie =<<)
-      
-beginsL :: Ord a => [a] -> [[a]] -> Trie a
-beginsL l = fromList . filter (L.isPrefixOf l)
-      
-insert :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
-insert = foldr f toTrue where
-  f e a = overMap (M.alter (Just . a . toTrie) e)
-
-null :: Trie a -> Bool
-null (Trie m e) = M.null m
-
 empty :: Trie a
 empty = Trie M.empty False
 
+insert :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
+insert = foldr f toTrue where
+  f e a = overMap (M.alter (Just . a . fromMaybe empty) e)
+
+instance Show a => Show (Trie a) where
+  show  = show . toList
+
+fromList :: (Ord a, Foldable f, Foldable g) => f (g a) -> Trie a
+fromList = foldr insert empty
+
+toList :: Trie a -> [[a]]
+toList (Trie m a) | a         = [] : rest
+                  | otherwise = rest where
+                  rest = M.assocs m >>= uncurry (fmap . (:)) . fmap toList
+
+contains :: (Ord a, Foldable f) => f a -> Trie a -> Bool
+contains = zipUntil False endHere
+
+hasPref :: (Ord a, Foldable f) => f a -> Trie a -> Bool
+hasPref = zipUntil False (const True)
+
+hasSuff :: (Ord a, Foldable f) => f a -> Trie a -> Bool
+hasSuff xs t = contains xs t || any (hasSuff xs) (getTrie t)
+
+complete :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
+complete = zipUntil empty id
+  
+begins :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
+begins xs = fromMaybe empty . begins' xs . Just where
+  begins' = foldr f id
+  f e a   = (fmap (flip Trie False . M.singleton e) . a . M.lookup e . getTrie =<<)
+  
+hasSub :: (Ord a, Foldable f) => f a -> Trie a -> Bool
+hasSub xs t = hasPref xs t || any (hasSub xs) (getTrie t)
+
+overMap :: Ord b => (M.Map a (Trie a) -> M.Map b (Trie b)) -> Trie a -> Trie b
+overMap f (Trie m e) = Trie (f m) e
+
+zipUntil :: (Ord a, Foldable f) => b -> (Trie a -> b) -> f a -> Trie a -> b
+zipUntil base = foldr f where
+  f e a = fromMaybe base . fmap a . M.lookup e . getTrie 
+
 toTrue :: Trie a -> Trie a
 toTrue (Trie m _) = Trie m True
-
-toTrie :: Maybe (Trie a) -> Trie a
-toTrie = fromMaybe empty
-
-toFalse :: Trie a -> Trie a
-toFalse (Trie m _) = Trie m False
-
-nilIfEmpty :: Trie a -> Maybe (Trie a)
-nilIfEmpty t@(Trie m e) | M.null m && not e = Nothing
-                        | otherwise         = Just t
-             
-someIfEmpty :: Trie a -> Maybe (Trie a)
-someIfEmpty t | null t = Just t
-              | otherwise = Nothing
-
-             
-getSubs :: (Ord a, Foldable f) => (Trie a -> Maybe (Trie a)) -> 
-                                  (Trie a -> Maybe (Trie a)) -> 
-                                   f a -> Trie a -> Trie a
-getSubs b g xs = toTrie . getSubs' xs where
-  getSubs' = foldr f b
-  f e a = g . overMap (M.mapMaybeWithKey ff) where
-    ff k v | k == e    = a v <|> getSubs' xs v
-           | otherwise = getSubs' xs v
            
 minit :: String -> String
 minit [] = []
@@ -110,23 +71,7 @@ debugPrint = debugPrint' "" where
       f (h,t) = str ++ debugPrint' (pad ++ b) t where
         str = show h
         pad = ' ' : zipWith const (repeat ' ') str
-           
---wiSubs :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
---wiSubs = getSubs nilIfEmptyEnd (nilIfEmptyEnd . toFalse)
---
---ends :: (Ord a, Foldable f) => f a -> Trie a -> Trie a
---ends = getSubs someIfEmpty (nilIfEmptyEnd . toFalse)
-           
-
-
-fromList :: (Ord a, Foldable f, Foldable g) => f (g a) -> Trie a
-fromList = foldr insert empty
-
-toList :: Trie a -> [[a]]
-toList (Trie m a) | a         = [] : rest
-                  | otherwise = rest where
-                  rest = M.assocs m >>= uncurry (fmap . (:)) . fmap toList
-                  
+            
 count :: Trie a -> Int
 count (Trie m e) = M.foldr ((+) . count) (if e then 1 else 0) m
 
@@ -135,5 +80,28 @@ instance Ord a => Ord (Trie a) where
                                                       EQ -> compare w z
                                                       GT -> GT
                                                       where (x,w) = M.findMax a
-                                                            (y,z) = M.findMax b
-                                                                                                                        
+                                                            (y,z) = M.findMax b                                                                                                                   
+
+containsL :: Eq a => [a] -> [[a]] -> Bool
+containsL = any . (==)
+
+completeL :: (Ord a) => [a] -> [[a]] -> Trie a
+completeL l = fromList . map (drop (length l)) . filter (L.isPrefixOf l)
+
+check :: (Ord a, Eq c) => ([a] -> Trie a -> c) -> ([a] -> [[a]] -> c) -> [[a]] -> Bool
+check tf lf l = all (liftM2 (==) tfa lfa) (liftM2 (++) L.tails L.inits =<< l)  where
+  t   = fromList l
+  tfa = flip tf t
+  lfa = flip lf l
+      
+beginsL :: Ord a => [a] -> [[a]] -> Trie a
+beginsL l = fromList . filter (L.isPrefixOf l)
+
+hasSubL :: Eq a => [a] -> [[a]] -> Bool
+hasSubL x l = any (L.isPrefixOf x) (l >>= L.tails)
+
+hasPrefL :: Eq a => [a] -> [[a]] -> Bool
+hasPrefL = any . L.isPrefixOf
+
+hasSuffL :: Eq a => [a] -> [[a]] -> Bool
+hasSuffL = any . L.isSuffixOf
